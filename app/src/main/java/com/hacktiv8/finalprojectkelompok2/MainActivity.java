d179107ec3498ede7a1c3c2891e0e6e502fb9ad0
AlphaP45 <alphapramadhani45@gmail.com>
2023-05-02 20:38:40 +0700

    Fp 1

---
+++ app/src/main/java/com/hacktiv8/finalprojectkelompok2/MainActivity.java
@@ -0,0 +1,77 @@
+package com.hacktiv8.finalprojectkelompok2;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+import Adapter.Adapters;
+import Database.Database;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList id, title, desc;
+    private Database database;
+    private Adapters rvAdapters;
+    private RecyclerView rvView;
+    private FloatingActionButton fab;
+
+    // decorations view
+    private ImageView decor1;
+    private TextView decor2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvView = findViewById(R.id.rvView);
+        database = new Database(MainActivity.this);
+        id = new ArrayList();
+        title = new ArrayList();
+        desc = new ArrayList();
+        decor1 = (ImageView) findViewById(R.id.imageView);
+        decor2 = (TextView) findViewById(R.id.acc2);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ViewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        getDataArray();
+        rvAdapters = new Adapters(MainActivity.this, this, id, title, desc);
+        rvView.setAdapter(rvAdapters);
+        rvView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    private void getDataArray() {
+        Cursor cursor = database.readData();
+        System.out.println(cursor.getCount());
+        if (cursor.getCount() > 0) {
+            decor1.setVisibility(View.GONE);
+            decor2.setVisibility(View.GONE);
+        }
+        while (cursor.moveToNext()) {
+            id.add(cursor.getString(0));
+            title.add(cursor.getString(1));
+            desc.add(cursor.getString(2));
+        }
+    }
+
+}
